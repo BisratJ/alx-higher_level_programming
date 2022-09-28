7270c6dfade3639c4c2b69e3f98d0ec66e39cf6f
Bisrat7 <bisrojc60@gmail.com>
2022-09-29 00:46:58 +0300

    returns the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-to_json_string.py
+# Bisrat Gizaw
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
