80b14e2087dac4cca8c8831519497a64cec07731
Bisrat Gizaw <bisrojc60@gmail.com>
2024-03-06 00:54:38 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
 if (process.argv.length > 2) {
-  fs.readFile(process.argv[2], (err, data) => {
+  let data;
+  fs.readFile(process.argv[2], (err, _data) => {
     if (err) {
-      console.log(err);
+      // Improve error message:
+      console.error(`Error reading file "${process.argv[2]}":`, err);
     } else {
+      data = _data;
       console.log(data.toString('utf-8'));
     }
   });
