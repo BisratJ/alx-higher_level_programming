a6ce5d1e8ce139dd22dc69b7de8c3a111ec76f03
BisratJ <bisrojc60@gmail.com>
2024-03-09 09:45:47 -0800

    adding some files

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Arguments: arg1: repo name, arg2: repo owner
+"""Python script that sends a request to the URL and
+   displays the value of a variable in the response header
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    # Get the arguments, arg1: repo name, arg2: repo owner
     url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2], sys.argv[1])
+        sys.argv[2], sys.argv[1])
 
     r = requests.get(url)
     commits = r.json()
     try:
         for i in range(10):
-            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
